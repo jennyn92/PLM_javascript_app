14ba9e9af9b5d64c626e0a1fd739467a738206d4
jennyn92 <134844026+jennyn92@users.noreply.github.com>
2024-03-15 09:10:23 +0000

    added sidebar

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ function addStyleContainer(){
         document.getElementById("styleContainerWrapper").appendChild(newStyleContainer);
     }
 }
+
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage(){
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.textContent= ""
+}
+
 //     if(existingStyleContainer){
 //         var newStyleContainer = document.createElement('div')
 //         newStyleContainer.innerHTML = existingStyleContainer.firstElementChild.innerHTML;
